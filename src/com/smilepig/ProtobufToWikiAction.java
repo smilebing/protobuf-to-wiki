9cf8f7825e34ab56c5d1fd6b27553274ad8d067e
zhuhe <zhuhe@changingedu.com>
2020-08-28 23:00:40 +0800

    add protobuf to wiki action

---
+++ src/com/smilepig/ProtobufToWikiAction.java
@@ -0,0 +1,47 @@
+package com.smilepig;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.SelectionModel;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.ui.popup.Balloon.Position;
+import com.intellij.openapi.ui.popup.BalloonBuilder;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.ui.JBColor;
+
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+
+/**
+ * Created by zhuhe on 2020/8/28
+ */
+public class ProtobufToWikiAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        Project project = e.getData(PlatformDataKeys.PROJECT);
+
+        Editor editor = e.getData(PlatformDataKeys.EDITOR);
+        assert editor != null;
+        SelectionModel selectionModel = editor.getSelectionModel();
+        String selectedText = selectionModel.getSelectedText();
+
+        String name = Messages.showInputDialog(project, "请输入域账号2", "提示", Messages.getQuestionIcon());
+        Messages.showMessageDialog(project, "登陆成功", "提示", Messages.getInformationIcon());
+
+        JBPopupFactory factory = JBPopupFactory.getInstance();
+        BalloonBuilder htmlTextBalloonBuilder = factory.createHtmlTextBalloonBuilder("内容", null, JBColor.PINK, new HyperlinkListener() {
+            @Override
+            public void hyperlinkUpdate(HyperlinkEvent e) {
+                System.out.println("hyper link");
+            }
+        });
+
+        htmlTextBalloonBuilder.setFadeoutTime(5 * 1000)
+                .createBalloon()
+                .show(factory.guessBestPopupLocation(editor), Position.below);
+    }
+}
